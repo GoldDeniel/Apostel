5494ff323a09a33322b0eb1b382204e6ad8506e3
GoldDeniel <aranyadhun@gmail.com>
2024-04-19 21:49:14 +0200

    Removed Jimmy from index

--- index.php
+++ index.php
@@ -78,7 +78,40 @@
             </div>
           </div>
           <?php
-        include 'product_list.php'
+        
+        require_once 'credentials.php';
+        $conn = new PDO(
+          'mysql:host=localhost;dbname='.DB_NAME.';charset=utf8',
+          DB_NAME,
+          DB_PASSWORD
+      );
+        $sql = "SELECT * FROM Beers ORDER BY id LIMIT 6";
+
+        $res = $conn -> query($sql);
+        $records = $res -> fetchAll(PDO::FETCH_ASSOC);
+        //var_dump($records);
+        
+        foreach ($records as $record) {
+
+          $prize = $record['price'] == 0 ? "<span style=\"color: red\">Out of stock</span>" : "$".$record['price'];
+
+          echo 
+          "
+            <div class=\"col-md-4\">
+              <div class=\"product-item\">
+                <a href=\"product-details.php\"><img src=\"assets/images/".$record['img_url']."\"></a>
+                <div class=\"down-content\">
+                  <a href=\"product-details.php\"><h4>".$record['label']."</h4></a>
+                  <h6>".$prize."</h6>
+                  <p>".$record['description']."</p>
+                </div>
+              </div>
+            </div>
+          ";
+        }
+        // $conn -> close();
+        
+        $conn = null;
         ?>
 
     <div class="best-features">
