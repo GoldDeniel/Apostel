09538f1ac4ff2c1e3632ccc835ea22ec406118e5
GoldDeniel <aranyadhun@gmail.com>
2024-05-01 15:39:02 +0200

    :rocket: : get_connection()

--- credentials.php
+++ credentials.php
@@ -1,4 +1,14 @@
 <?php
- 
+
     define('DB_NAME', 'ApostHell');
-    define('DB_PASSWORD', 'valaminekkakiszagavanetetetetetete');
+    define('DB_PASSWORD', 'AVP*)X06nI.*uy)Y');
+    session_start();
+    // report and write php errors
+
+    function get_connection() {
+        return new PDO(
+            'mysql:host=localhost;dbname='.DB_NAME.';charset=utf8',
+            DB_NAME,
+            DB_PASSWORD
+        );
+    }
